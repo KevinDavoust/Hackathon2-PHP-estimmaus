d05dd6b53aa75716998ba1bd1cd01265534266f8
KevinDavoust <kevin.davoust@orange.fr>
2023-06-30 09:14:10 +0200

    fix faq

--- migrations/Version20230630070329.php
+++ migrations/Version20230630070329.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230629235002 extends AbstractMigration
+final class Version20230630070329 extends AbstractMigration
 {
     public function getDescription(): string
     {
